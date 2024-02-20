09861f909941a2f472d739725f30e7eaa7412c25
Nicolas Romano <nicoezequielromano98@gmail.com>
2024-02-20 12:11:45 -0300

    reentrega

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const app = express();
 const PORT = 8080;
 
 app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
 
 app.use("/api/products", productRouter);
 app.use("/api/carts", cartRouter);
+
+
 
 app.listen(PORT, () => {
     console.log(`Server run on port ${PORT}`);
